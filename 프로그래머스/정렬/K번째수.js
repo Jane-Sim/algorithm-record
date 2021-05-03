87653121d26c031559ea03bc505849903ecccdde
seyoung.sim <ssiox3@gmail.com>
2021-05-03 18:27:11 +0900

    정렬인 K번째수에서 다른 사람 문제풀이 추가

--- 프로그래머스/정렬/K번째수.js
+++ 프로그래머스/정렬/K번째수.js
@@ -45,3 +45,9 @@ console.log(
     ]
   )
 );
+
+/** 다른 사람의 풀이 */
+function solution(numbers) {
+  let answer = numbers.sort((a, b) => `${b}${a}` - `${a}${b}`).join("");
+  return answer[0] === "0" ? "0" : answer;
+}
